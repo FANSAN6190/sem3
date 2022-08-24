537458fd52373a2a07adc39a0f11ae4c6e8d2b96
FANSAN6190 <fanin.s.pbl@gmail.com>
2022-08-25 00:33:41 +0530

    ap le2

--- AP/Lab2/le2q17.py
+++ AP/Lab2/le2q17.py
@@ -1,3 +1,9 @@
 #17. Write a program to prints the integer cube root, if it exists, of an integer. If the input is not a
 #    perfect cube, it prints a message “the number is not perfect cube” otherwise it prints “the number is
 #    perfect cube”.
+n=int(input(" "))
+r=round(n**(1/3))
+if r**3==n:
+    print("The number is perfect cube. ",r)
+else:
+    print("The number is not perfect cube.")
---
+++ AP/Lab2/le2q19.py
@@ -0,0 +1,3 @@
+#19.  Write a program to print all odd number between 1 to 100.
+for i in range(1,100,2):
+	print(i)
